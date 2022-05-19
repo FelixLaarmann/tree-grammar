e0b7937be24b07cb7a760a8c2fbc4248b3e00068
felix.laarmann <felix.laarmann@tu-dortmund.de>
2022-05-19 15:30:22 +0200

    init forAnna branch

--- app/Main.hs
+++ app/Main.hs
@@ -6,36 +6,77 @@ import Grammar
 import Examples
 import CLSoutput
 
-import Control.Unification.IntVar
-import Control.Unification.Types
-import qualified Data.Map as Map
-import qualified Data.Set as Set
 import Data.List
 import Control.Monad
 
+{-
+boolGrammar corresponds to grammar G_B from Example 1 in section 8.1 of the paper.
+boolRS' corresponds to the linear version of the rewriting system RS^lin_B from Example 1.
+boolRS corresponds to the non-linear version.
+-}
+interBool = reduce $ simplifyStates $ productADC (constructADC boolGrammar) (constructNfADC boolRS')
+
+{-
+sortGrammar corresponds to grammar G_sort from Example 2 in section 8.2 of the paper.
+sortRS' corresponds to the linear version of the rewriting system RS^lin_sort from Example 2.
+sortRS  corresponds to the non-linear version.
+-}
+interSort = reduce $ simplifyStates $ productADC (constructADC sortGrammar) (constructNfADC sortRS')
+
+{-
+clsGrammar is an adopted CLS-generated grammar for a 30x30 labyrinth.
+cslRS corrresponds to RS_CLS from Example 3 in section 8.3 of the paper.
+-}
+interCLS = reduce $ simplifyStates $ productADC (constructADC clsGrammar) (constructNfADC $ clsRS)
 
 main :: IO ()
 main = do
-  putStrLn "BOOL intersection is empty:"
+  {-
+  ####
+  Example 1
+  ####
+  -}
+  --prettyPrintADC interBool
+  --putStrLn ""
+  putStrLn "intersection language of boolGrammar and linear RS is empty:"
   print $ ftaEmptiness interBool
-  putStrLn "BOOL intersection is finite:"
+  putStrLn "intersection language of boolGrammar and linear RS is finite:"
   print $ ftaFiniteness interBool
-  putStrLn "SORT intersection is empty:"
+  {-
+  --be careful, the following non-linear version will need more time, than anyone of us has ;P
+  putStrLn "intersection language of boolGrammar and non-linear RS is empty:"
+  print $ intersectionIsEmpty boolGrammar boolRS'
+  putStrLn "intersection language of boolGrammar and non-linear RS is finite:"
+  print $ intersectionIsFin boolGrammar boolRS'
+  -}
+
+  {-
+  ####
+  Example 2
+  ####
+  -}
+  --prettyPrintADC interSort
+  --putStrLn ""
+  putStrLn "intersection language of sortGrammar and linear RS is empty:"
   print $ ftaEmptiness interSort
-  putStrLn "SORT intersection is finite:"
+  putStrLn "intersection language of sortGrammar and linear RS is finite:"
   print $ ftaFiniteness interSort
-  putStrLn "CLS Labyrinth intersection is empty:"
+  {-
+  --non-linear version:
+  putStrLn "intersection language of sortGrammar and non-linear RS is empty:"
+  print $ intersectionIsEmpty sortGrammar sortRS
+  putStrLn "intersection language of boolGrammar and non-linear RS is finite:"
+  print $ intersectionIsFin sortGrammar sortRS
+  -}
+  
+  {-
+  ####
+  Example 3
+  ####
+  -}
+  --prettyPrintADC interCLS
+  --putStrLn ""
+  putStrLn "intersection language of clsGrammar and clsRS is empty:"
   print $ ftaEmptiness interCLS
-  putStrLn "CLS Labyrinth intersection is finite:"
+  putStrLn "intersection language of clsGrammar and clsRS is finite:"
   print $ ftaFiniteness interCLS
-
-
-interBool = productADC (constructADC boolGrammar) (constructNfADC boolRS')
-interSort = simplifyStates $ reduce $ productADC (constructADC sortGrammar) (constructNfADC sortRS')
-
-labG = labyrinthGrammar 3 4 [(0,0), (1,2), (2,0)] (0,2) (1,0)
-labG' = labyrinthGrammar 10 10 [(0,0), (1,2), (2,0), (5,5), (5,6), (8,9)] (1,0) (9,9)
-
-interLab = simplifyStates $ productADC (constructADC labG') (constructNfADC labyrinthRS)
-
-interCLS = simplifyStates $ productADC (constructADC clsGrammar) (constructNfADC $ clsRS)
